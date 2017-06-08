efdfef1fa8c3e94523e845e8a785168883f0c8e9
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-08 15:28:56 +0800

    Updated dependency.

--- aply.js
+++ aply.js
@@ -33,6 +33,9 @@
 			"module": "aply",
 			"author": "Richeve S. Bebedor",
 			"eMail": "richeve.bebedor@gmail.com",
+			"contributors": [
+				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>"
+			],
 			"repository": "https://github.com/volkovasystems/aply.git",
 			"test": "aply-test.js",
 			"global": true
