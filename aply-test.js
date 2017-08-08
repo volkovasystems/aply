fcc85185c09947bb566d0dadfcd38602f7034bb6
johnlenon <johnlenonmaghanoy@gmail.com>
2017-08-08 13:37:36 +0800

    Updated test.

--- aply-test.js
+++ aply-test.js
@@ -2,20 +2,30 @@
 const assert = require( "assert" );
 const aply = require( "./aply.js" );
 
-assert.deepEqual( aply( function hello( value ){
-	return [ "hey", value, this ];
-}, { "hello": "world" }, "yeah" ), [ "hey", "yeah", { "hello": "world" } ], "should be deeply equal" );
+assert.deepEqual( aply(
+		function hello( value ){ return [ "hey", value, this ]; },
+			{ "hello": "world" }, "yeah"
+	),
+	[ "hey", "yeah", { "hello": "world" } ],
+	"should be equal to [ 'hey', 'yeah', { 'hello': 'world' } ]" );
 
-assert.deepEqual( aply( function hello( value, data ){
-	return [ "hey", value, data, this ];
-}, { "hello": "world" }, "yeah", 123 ), [ "hey", "yeah", 123, { "hello": "world" } ], "should be deeply equal" );
+assert.deepEqual( aply(
+	function hello( value, data ){ return [ "hey", value, data, this ]; },
+		{ "hello": "world" }, "yeah", 123 ),
+	[ "hey", "yeah", 123, { "hello": "world" } ],
+	"should be equal to [ 'hey', 'yeah', 123, { 'hello': 'world' } ]" );
 
-assert.deepEqual( aply( function test( parameter ){
-	return Array.from( arguments );
-}, "test", [ "hello", [ "world" ], "yeah" ] ), [ "hello", [ "world" ], "yeah" ], "should be deeply equal" );
+assert.deepEqual( aply(
+	function test( parameter ){ return Array.from( arguments ); },
+		"test", [ "hello", [ "world" ], "yeah" ] ),
+	[ "hello", [ "world" ], "yeah" ],
+	"should be equal to [ 'hello', [ 'world' ], 'yeah' ]" );
 
-assert.deepEqual( aply( function test( parameter ){
-	return Array.from( arguments );
-}, "test", [ "hello", "world", "yeah" ] ), [ "hello", "world", "yeah" ], "should be deeply equal" );
+assert.deepEqual( aply(
+	function test( parameter ){
+		return Array.from( arguments );
+	}, "test", [ "hello", "world", "yeah" ] ),
+	[ "hello", "world", "yeah" ],
+	"should be equal to [ 'hello', 'world', 'yeah' ]" );
 
 console.log( "ok" );
